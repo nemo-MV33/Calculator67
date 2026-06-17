c9f36ffd0a04050bbda09d721132f6fb2bb95d7f
xster787 <xster787@gmail.com>
2026-06-17 11:24:59 +0800

    расширение списка проверки приложения

--- tests.cpp
+++ tests.cpp
@@ -65,3 +65,131 @@ TEST(WhiteBox, Skb) {
 TEST(WhiteBox,Nothing){
     EXPECT_THROW(calc("++"),runtime_error);
 }
+
+TEST(BlackBox, UnaryMinus) {
+    EXPECT_DOUBLE_EQ(calc("-5+3"), -2.0);
+}
+TEST(BlackBox, Float) {
+    EXPECT_DOUBLE_EQ(calc("3.5*2"), 7.0);
+}
+TEST(BlackBox, NestedParentheses) {
+    EXPECT_DOUBLE_EQ(calc("((2+3))*2"), 10.0);
+}
+TEST(BlackBox, RootAndPower) {
+    EXPECT_DOUBLE_EQ(calc("√9+2^3"), 11.0);
+}
+TEST(BlackBox, EmptyExpression) {
+    EXPECT_THROW(calc(""), runtime_error);
+}
+
+TEST(WhiteBox, DoubleUnaryMinus) {
+    EXPECT_DOUBLE_EQ(calc("--5"), 5.0);
+}
+TEST(WhiteBox, LongChain) {
+    EXPECT_DOUBLE_EQ(calc("1+2+3+4"), 10.0);
+}
+
+//new tests
+TEST(BlackBox, LargeNumbers) {
+    EXPECT_DOUBLE_EQ(calc("2000000000*2000000000"), 4000000000000000000.0);
+}
+
+TEST(BlackBox, DotWithoutDigitAfter) {
+    EXPECT_DOUBLE_EQ(calc("2.*4"), 8.0);
+}
+
+TEST(BlackBox, LeadingDot) {
+    EXPECT_DOUBLE_EQ(calc(".5+.5"), 1.0);
+}
+
+
+TEST(BlackBox, ZeroResult) {
+    EXPECT_DOUBLE_EQ(calc("0*1000"), 0.0);
+}
+
+TEST(BlackBox, NegativePower) {
+    EXPECT_DOUBLE_EQ(calc("2^-1"), 0.5);
+}
+
+TEST(BlackBox, ZeroPowerZero) {
+    EXPECT_DOUBLE_EQ(calc("0^0"), 1.0);
+}
+
+TEST(BlackBox, RootOfZero) {
+    EXPECT_DOUBLE_EQ(calc("√0"), 0.0);
+}
+
+TEST(BlackBox, ManyNestedParentheses) {
+    EXPECT_DOUBLE_EQ(calc("((((5))))"), 5.0);
+}
+
+TEST(BlackBox, EmptyParentheses) {
+    EXPECT_THROW(calc("()"), runtime_error);
+}
+
+TEST(BlackBox, OnlyOperator) {
+    EXPECT_THROW(calc("*"), runtime_error);
+}
+
+TEST(WhiteBox, Fraction) {
+    EXPECT_NEAR(calc("1/3"), 0.333333, 1e-5);
+}
+TEST(WhiteBox,SigedBeforSkobka) {
+    EXPECT_THROW(calc("5("), runtime_error);
+}
+
+TEST(WhiteBox, ComboSym) {
+    EXPECT_THROW(calc("5√9"), runtime_error);
+}
+
+TEST(WhiteBox, TestFormata) {
+    EXPECT_THROW(calc("(√)"), runtime_error);
+}
+
+TEST(BlackBox, Overflow) {
+    EXPECT_TRUE(isinf(calc("10000000000^10000000000")));
+}
+
+TEST(BlackBox, RootOfRoot) {
+    EXPECT_DOUBLE_EQ(calc("√(√16)"), 2.0);
+}
+
+TEST(BlackBox, NegativeRoot) {
+    
+    EXPECT_DOUBLE_EQ(calc("-√9"), -3.0);
+}
+
+TEST(BlackBox, ZeroDividedByNumber) {
+    EXPECT_DOUBLE_EQ(calc("0/5"), 0.0);
+}
+
+TEST(BlackBox, SmallNumbers) {
+    EXPECT_NEAR(calc("0.000001*1000000"), 1.0, 1e-9);
+}
+
+TEST(BlackBox, OneToAnyPower) {
+    EXPECT_DOUBLE_EQ(calc("1^99999"), 1.0);
+}
+
+TEST(BlackBox, NegativeZero) {
+    EXPECT_DOUBLE_EQ(calc("-0"), 0.0);
+}
+
+TEST(BlackBox, RootWithoutNumber) {
+    EXPECT_THROW(calc("√"), runtime_error);
+}
+
+TEST(BlackBox, PowerWithoutExponent) {
+    EXPECT_THROW(calc("5^"), runtime_error);
+}
+
+TEST(BlackBox, PowerWithoutBase) {
+    EXPECT_THROW(calc("^5"), runtime_error);
+}
+
+TEST(BlackBox, DoubleOperator) {
+    EXPECT_THROW(calc("5++3"), runtime_error);
+}
+TEST(WhiteBox, NegativeRoot) {
+    EXPECT_TRUE(isnan(calc("√-4")));
+}
